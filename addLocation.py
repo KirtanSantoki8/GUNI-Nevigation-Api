2421e71c455ead512ffcf55dd556954d8ce33c65
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-04-04 20:12:08 +0530

    Commit

---
+++ addLocation.py
@@ -0,0 +1,10 @@
+import sqlite3
+import uuid
+
+def addLocation(image,name):
+    conn = sqlite3.connect('GUNI Nevigation.db')
+    cur = conn.cursor()
+    location_id = str(uuid.uuid4())
+    cur.execute('''INSERT INTO location (location_id,location_thumbnail,location) VALUES (?,?,?)''',(location_id,image,name))
+    conn.commit()
+    conn.close()
