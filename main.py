ea99a62279dd2b1f72a3b11d827b51065b00d824
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-04-05 09:43:20 +0530

    commit

--- main.py
+++ main.py
@@ -69,8 +69,7 @@ def upload_location():
         location = request.form['location']
 
         thumbnail = getImage(image_data)
-        print(thumbnail)
-        location_id = addLocation(thumbnail,location)
+        location_id = addLocation(str(thumbnail),location)
         
         return jsonify({'message':str(location_id),'status':200})
     
