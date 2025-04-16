c28cee226354ba496b0702fd4ed653c6c45efcfd
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-04-16 13:14:50 +0530

    commit

--- allSubLocations.py
+++ allSubLocations.py
@@ -15,7 +15,7 @@ def uploadSubPlaces(image,name,mainLocation,longitude,latitude):
     cur = conn.cursor()
     sub_location_id = str(uuid.uuid4())
     dateOfCreation = date.today()
-    cur.execute('''INSERT INTO sub_locations (sub_location_id,main_location_name,sub_location_thumbnail,sub_location_name,longitude,latitude,date_of_sub_location_creation) VALUES (?,?,?,?,?)''',(sub_location_id,mainLocation,image,name,longitude,latitude,dateOfCreation))
+    cur.execute('''INSERT INTO sub_locations (sub_location_id,main_location_name,sub_location_thumbnail,sub_location_name,longitude,latitude,date_of_sub_location_creation) VALUES (?,?,?,?,?,?,?)''',(sub_location_id,mainLocation,image,name,longitude,latitude,dateOfCreation))
     conn.commit()
     conn.close()
 
