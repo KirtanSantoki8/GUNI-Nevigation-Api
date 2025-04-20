b9b50e57711118abd13d26577e41ca99df04e665
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-04-20 22:12:01 +0530

    commit

--- deleteData.py
+++ deleteData.py
@@ -6,6 +6,7 @@ def delete_location_cateory(location):
         cursor = conn.cursor()
         
         cursor.execute("DELETE FROM location WHERE location = ?", (location,))
+        cursor.execute("DELETE FROM main_locations WHERE main_location_names = ?", (location,))
         
         conn.commit()
         conn.close()
